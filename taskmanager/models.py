d771e7f6535a3640307e718e5f8af08148d31474
EdgarasSp <dev.applications.esp@gmail.com>
2022-06-04 23:49:43 +0000

    add fdisplay added category

--- taskmanager/models.py
+++ taskmanager/models.py
@@ -4,7 +4,7 @@ from taskmanager import db
 class Category(db.Model):
     # schema for the Category model
     id = db.Column(db.Integer, primary_key=True)
-    category_name = db.Column(db.String(25), unique=True, nullable=False) # nullable means required field
+    category_name = db.Column(db.String(25), unique=True, nullable=False)
     tasks = db.relationship("Task", backref="category", cascade="all, delete", lazy=True)
 
     def __repr__(self):
